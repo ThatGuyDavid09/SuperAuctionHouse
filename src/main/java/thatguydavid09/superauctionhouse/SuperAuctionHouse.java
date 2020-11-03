d07708d84a0f36ef275a351d86d5026add0ceb05
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-11-03 18:35:19 -0500

    Initial commit

---
+++ src/main/java/thatguydavid09/superauctionhouse/SuperAuctionHouse.java
@@ -0,0 +1,17 @@
+package thatguydavid09.superauctionhouse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SuperAuctionHouse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
