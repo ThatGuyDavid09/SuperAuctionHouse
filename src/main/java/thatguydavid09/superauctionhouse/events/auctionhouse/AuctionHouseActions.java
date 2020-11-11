073cab5a255d6e6c1122b4319563e6e9fdc6e8f5
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-11-10 20:11:10 -0500

    Started work on logic to fix pages not turning

--- src/main/java/thatguydavid09/superauctionhouse/events/auctionhouse/AuctionHouseActions.java
+++ src/main/java/thatguydavid09/superauctionhouse/events/auctionhouse/AuctionHouseActions.java
@@ -7,15 +7,16 @@ public class AuctionHouseActions {
     // FIXME why doesn't this work
     // Flip to next auction house page
     public static void nextPage(Player player) {
-        int indexOfInv = BaseAuctionHouseMenu.auctionHousePages.indexOf(player.getOpenInventory().getTopInventory());
+        // This monster piece of crap code gets the current index of the ah we are on by getting the name of the forward and back arrows I know it is garbage
+        int currentIndexOfInv = Integer.parseInt(player.getOpenInventory().getItem(50).getItemMeta().getDisplayName().split("/")[0].split(" ")[3]) - 1;
         player.closeInventory();
-        player.openInventory(BaseAuctionHouseMenu.auctionHousePages.get(indexOfInv + 1));
+        player.openInventory(BaseAuctionHouseMenu.auctionHousePages.get(currentIndexOfInv + 1));
     }
 
     // Flip to previous auction house page
     public static void previousPage(Player player) {
-        int indexOfInv = BaseAuctionHouseMenu.auctionHousePages.indexOf(player.getOpenInventory().getTopInventory());
+        int currentIndexOfInv = Integer.parseInt(player.getOpenInventory().getItem(48).getItemMeta().getDisplayName().split("/")[0].split(" ")[3]);
         player.closeInventory();
-        player.openInventory(BaseAuctionHouseMenu.auctionHousePages.get(indexOfInv - 1));
+        player.openInventory(BaseAuctionHouseMenu.auctionHousePages.get(currentIndexOfInv - 1));
     }
 }
