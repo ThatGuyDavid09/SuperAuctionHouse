3668567f1a8d29463db7e2e5553fb079df449b24
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-11-27 09:40:50 -0500

    Fixed error when player tried to sell empty hand

--- src/main/java/thatguydavid09/superauctionhouse/commands/SellCommand.java
+++ src/main/java/thatguydavid09/superauctionhouse/commands/SellCommand.java
@@ -11,8 +11,14 @@ import thatguydavid09.superauctionhouse.menus.auctionhouse.BaseAuctionHouseMenu;
 
 public class SellCommand {
     public static boolean sell(Player player, String[] args) {
-        BaseAuctionHouseMenu.addItem(player.getInventory().getItemInMainHand(), player, Long.parseLong(args[1]));
         ItemStack soldItem = player.getInventory().getItemInMainHand();
+
+        if (soldItem.getAmount() == 0) {
+            player.sendMessage(ChatColor.RED + "Please put your hotbar cursor over the item you want to sell!");
+            return true;
+        }
+
+        BaseAuctionHouseMenu.addItem(player.getInventory().getItemInMainHand(), player, Long.parseLong(args[1]));
 
         if (Strings.isNullOrEmpty(soldItem.getItemMeta().getDisplayName())) {
             player.sendMessage(ChatColor.GREEN + "You have sold " + ChatColor.GOLD + soldItem.getAmount() + " " + WordUtils.capitalizeFully(String.valueOf(soldItem.getType()).replace("_", " ")) + ((soldItem.getAmount() != 1) ? "s" : "") + ChatColor.GREEN + " for " + ChatColor.GOLD + args[1]);
